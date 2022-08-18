94a6875701e84a38b9323ad381f9fc621a72f599
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-08-18 15:05:47 +0100

    moved

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn excel_comp.wsgi --log-file--
+web: gunicorn excel_comp.wsgi
--- excel_comp/settings.py
+++ excel_comp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "whitenoise.runserver_nostatic",
+    "gunicorn",
 
     # local apps
     'homepage',
